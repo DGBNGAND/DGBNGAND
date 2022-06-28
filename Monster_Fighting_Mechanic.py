0b7d712e3c458ab6c72c87301125ae74d6adc3b2
DGBNGAND <99659522+DGBNGAND@users.noreply.github.com>
2022-06-28 14:24:25 +0100

    Update Monster_Fighting_Mechanic.py

--- Monster_Fighting_Mechanic.py
+++ Monster_Fighting_Mechanic.py
@@ -2,16 +2,16 @@ import random
 import time
 c=0
 starter=[]
-class poke_attack:
+class monster_attack:
     def __init__(self,att_name,att_power):
         self.att_name=att_name
         self.att_power=att_power
-tackle=poke_attack('Tackle',(1,2,3))
-scratch=poke_attack('Scratch',(1,2,3))
-ember=poke_attack('Ember',(5,6,7))
-razor_leaf=poke_attack('Razor Leaf',(5,6,7))
-aqua_tail=poke_attack('Aqua Tail',(3,4,5))
-class pokemon:
+bodyslam=monster_attack('Body Slam',(1,2,3))
+scratch=monster_attack('Scratch',(1,2,3))
+burn=monster_attack('Burn',(5,6,7))
+bloom=monster_attack('Bloom',(5,6,7))
+water_slap=poke_attack('Water Slap',(3,4,5))
+class monster:
     def __init__(self,name,attack,health,description):
         self.name = name
         self.attack = attack
@@ -20,38 +20,34 @@ class pokemon:
 def fight(target,attack):
     target.health=target.health-attack
     return target   
-charmander=pokemon('Charmander',5.2,39,'The flame that burns at the tip of its tail is an indication of its emotions.')
-bulbasaur=pokemon('Bulbasaur',4.9,45,'Bulbasaur is a cute Pokémon born with a large seed firmly affixed to its back;\
-the seed grows in size as the Pokémon does')
-squirtle=pokemon('Squirtle',4.8,44,'Squirtles shell is not merely used for protection.\
-The shells rounded shape and the grooves on its surface help minimize\n\
-resistance in water,\
-enabling this Pokémon to swim at high speeds.')
-starter.append(charmander)
-starter.append(bulbasaur)
-starter.append(squirtle)
+FlameBoy=monster('Flame Boy',5.2,39,'Fire Type Monster')
+LeafGuy=monster('Leaf Guy',4.9,45,'Leaf Type Monster')
+WaterDude=monster('Water Dude',4.8,44,'Water Type Monster')
+starter.append(FlameBoy)
+starter.append(LeafGuy)
+starter.append(WaterDude)
 def game():
     print('Please choose from one of these pokemon:\n')
-    print('[1] Charmander\n{}'.format(charmander.description))
-    print('[2] Bulbasaur\n{}'.format(bulbasaur.description))
-    print('[3] Squirtle\n{}\n'.format(squirtle.description))
-    poke_choice=int(input('->'))
+    print('[1] Charmander\n{}'.format(FlameBoy.description))
+    print('[2] Bulbasaur\n{}'.format(LeafGuy.description))
+    print('[3] Squirtle\n{}\n'.format(WaterDude.description))
+    monster_choice=int(input('->'))
     valid=False
     while not valid:
-        if poke_choice == 1:
-            my_pokemon=pokemon('Charmander',5.2,39,'The flame that burns at the tip of its tail is an indication of its emotions.')
+        if monster_choice == 1:
+            my_monster=FlameBoy
             valid=True
         elif poke_choice == 2:
-            my_pokemon=bulbasaur
+            my_pokemon=LeafGuy
             valid=True
         elif poke_choice == 3:
-            my_pokemon=squirtle
+            my_pokemon= WaterDude
             valid=True
         else:
             continue
     enemy=random.choice(starter)
-    battle(my_pokemon,enemy)
-def battle(my_pokemon,enemy):
+    battle(my_monster,enemy)
+def battle(my_monster,enemy):
     dead=False
     print('A wild {0} appeared!'.format(enemy.name))
     print('What do you choose to do :')
